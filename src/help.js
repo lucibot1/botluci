fbab9263c24bf1f431432f7216e821ce0820eb16
lucibot1 <knduart0@gmail.com>
2021-02-06 17:06:22 -0300

    First commit

---
+++ src/help.js
@@ -0,0 +1,212 @@
+const help = (prefix) => {
+	return `
+▂▄▅▆▇█*luci BOT*█▇▆▅▄▂ 
+◆ *informações*
+  ◆➤🔰 Prefix🔰: ◆◆  ${prefix}  ◆◆
+  ◆➤ Criador🔰 ◆◆: LUCI_SENPAI 🔰 ◆◆
+░███████████░░░░
+🏿🏿🏿🏿🏿🏿🏿🏿
+🏿🏿🏿🏿🏿🏿🏿🏿
+🏿🏿🏽🏽🏽🏽🏿🏿
+🏽🏽🏽🏽🏽🏽🏽🏽
+🏽⬜⬛🏽🏽⬛⬜🏽
+🏽🏽🏽🏿🏿🏽🏽🏽
+🏽🏽🏿🏽🏽🏿🏽🏽
+🏽🏽🏿🏿🏿🏿🏽🏽
+ 🔰 ALGUNS COMANDOS NÃO ESTÃO FUNCIONANDO, SE VIM RECLAMAR É SIMPLES "PARE DE USAR" grato.🔰
+🔰canal do nabuto: https://youtube.com/channel/UCZEtf9AlsC2zsJQwrfW-44w
+DIGITE ${prefix}info PARA MAIS INFORMAÇÕES
+    🔰❦❧❦❧❦❧ - ❦❧❦❧❦❦❧❦🔰
+◆ CRIAR LINK PARA SEU WPP
+ ◆◆➤${prefix}wa.me
+ ◆◆➤${prefix}wame
+◆ *SOBRE*
+
+ ◆➤ $${prefix}owner
+      (Dono)
+ 
+ ◆➤ ${prefix}infonomor
+ ◆➤ ${prefix}blocklist
+     (lista de contatos bloqueados)
+ ◆➤ ${prefix}chatlist
+     (lista de chat)
+ ◆➤ ${prefix}ping
+     (cell)
+ ◆➤ ${prefix}bugreport
+     (bug report)
+ ◆◆➤${prefix}makermenu
+◆➤ *COMANDO 2*
+  
+ ◆➤ ${prefix}sticker
+     (fazer figurinha)
+ ◆➤ ${prefix} stickergif
+     (fazer figurinha animada)
+ 
+◆➤ ${prefix}covidcountry
+     (info covid)
+◆➤ ${prefix}toimg
+   (converte figurinha em imagem)
+ ◆➤ ${prefix}tomp3
+     (baixar áudio de um vídeo do *wpp* )
+ 
+ ◆➤ ${prefix}ttp
+      (converter texto em áudio)
+ ◆➤ ${prefix}shota
+ ◆➤ ${prefix}quotemaker
+ ◆➤ ${prefix}testine
+ ◆➤ ${prefix}semoji
+ 
+ ◆➤ ${prefix}animecry
+ ◆➤ ${prefix}bugreport
+      (reporta bug)
+ ◆ GRUPOS
+ ◆➤ ${prefix}ownergroup
+      (dono do grupo)
+ ◆➤ ${prefix}linkgroup ou linkgc
+      (link do grupo)
+ ◆➤ ${prefix}closegc
+      (fechar grupo)
+ ◆➤ ${prefix}opengc
+ ◆➤ ${prefix}kick @
+     (banir membro)
+ ◆➤ ${prefix}promote @
+      (dá adm)
+ ◆➤ ${prefix}demote @
+     (revaixar ADM)
+  
+ ◆+𝟭𝟴 HENTAI
+ ◆➤ ${prefix}randomhentai
+ ◆➤ ${prefix}hentai
+      (hentai né pae)😍
+ ◆➤ ${prefix}randomhentaio
+◆ *MÍDIA*
+  
+ ◆➤ ${prefix}trendtwit
+ ◆➤ ${prefix}randomk
+ ◆➤ ${prefix}ytsearch
+◆ *COMANDOS 3*
+  
+ ◆➤ ${prefix}wiki
+ ◆➤ ${prefix}wikien
+ ◆➤ ${prefix}nulis
+ ◆➤ ${prefix}quotes
+ ◆➤ ${prefix}quotes2
+ ◆➤ ${prefix}artinama
+◆ *MAGIC SHELL*
+  
+ ◆➤ ${prefix}apakah
+ ◆➤ ${prefix}kapankah
+ ◆➤ ${prefix}rate
+ ◆➤ ${prefix}bisakah
+
+◆ *DOWNLOADS*
+  
+ ◆➤ ${prefix}images
+ ◆➤ ${prefix}ytmp3
+ ◆➤ ${prefix}ytmp4
+ ◆➤ ${prefix}tiktok
+ ◆➤ ${prefix}joox
+◆ *meme*
+  
+ ◆➤ ${prefix}meme
+ ◆➤ ${prefix}memeindo
+◆ *SOM*
+  
+ ◆➤ ${prefix}play
+ ◆➤ ${prefix}baixar
+ ◆➤ ${prefix}}tts
+◆ *MÚSICA*
+  
+ ◆➤ ${prefix}lirik
+ ◆➤ ${prefix}chord
+◆ *ISLAM*
+  
+ ◆➤ ${prefix}quran
+◆ *STALK*
+  
+ ◆➤ ${prefix}tiktokstalk
+ ◆➤ ${prefix}igstalk
+◆ *WIBU*
+  
+  ◆ ANIMES
+ ◆➤ ${prefix}animehug
+ ◆➤ ${prefix}neonime
+      (anime)
+ ◆➤ ${prefix}pokemon
+     (foto pokémon)
+ ◆➤ ${prefix}loli
+     (foto loli)
+ ◆➤ ${prefix}}waifu
+      (waifu)
+ ◆➤ ${prefix}randomanime
+      (foto anime)
+ ◆➤ ${prefix}}husbu
+      (sla)
+ ◆➤ ${prefix}husbu2
+      (sla)
+ ◆➤ ${prefix}}wait
+      (pesquisar anime)
+ ◆➤ ${prefix}nekonime
+      (nekoanime)
+◆ *DIVERSÃO
+  │
+ ◆➤ ${prefix}alay
+ ◆➤ ${prefix}gantengcek
+ ◆➤ ${prefix}watak
+ ◆➤${prefix}hobby
+ ◆➤ ${prefix}game. *a maioria*       
+   c             *não fuciona*
+ ◆➤ ${prefix}trust
+ ◆➤ ${prefix}dare
+ ◆➤ ${prefix}simi
+◆ *INFORMAÇÃO*
+  
+ ◆➤ ${prefix}bahasa
+ ◆➤ ${prefix}kodenegara
+ ◆➤ ${prefix}kbbi
+ ◆➤ ${prefix}fakta
+ ◆➤ ${prefix}infocuaca
+ ◆➤ ${prefix}infogempa
+ ◆➤ ${prefix}jadwaltvnow
+ ◆➤ ${prefix}covid
+◆ *PROPRIETÁRIO*
+  
+ ◆➤ ${prefix}setprefix
+      (mudar o setprefix)
+ ◆➤ ${prefix}block
+      (bloquear)
+ ◆➤  ${prefix}bc
+      (transmissão)
+ ◆➤ ${prefix}bcgc
+      (transmissão so no grupo)
+ ◆➤ ${prefix}clone
+      (pegar perfil)
+ ◆➤ ${prefix}clearall
+      (apagar todas msg)
+◆ *OUTROS*
+   
+  ◆➤ ${prefix}randomkpop
+  ◆➤ ${prefix}send
+  ◆➤ ${prefix}virtex
+       (trava)
+  ◆➤ ${prefix}exe
+  ◆➤ ${prefix}qrcode
+  ◆➤ ${prefix}afk
+  ◆➤ ${prefix}timer
+  ◆➤ ${prefix}fml
+  ◆➤ ${prefix}fml2
+╭╭╮╭╭╮┈┈
+             ┃┃┃┃┃┃☆┈
+FIM.
+             ┃┃┃┃┃┃┈┈
+             ┃┛┻┛┛┃╭╮
+             ┃┈┈╱▔┃┃┃
+             ┃┈╱┈┈┗╯┃
+             ╰┳┊┊┳━━╯
+             ┏┻━━┻┓┈☆
+             ┃┈┈┈┈┃┈┈
+             ┃┈┈┈┈┃┈┈
+`
+}
+
+exports.help = help
---
+++ src/nabutomenu.js
@@ -0,0 +1,12 @@
+const lucimenu = (prefix, pushname) => {
+    return `◪ *Comandos do luci*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.lucimenu = lucimenu
